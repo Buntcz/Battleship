60f4c83e0e550aa3aff710774f61b2108ffcb4f7
Buntcz <testtigeryt@gmail.com>
2024-05-18 20:47:36 +0300

    Checked if all placeShip function restrictions are working. They do. Also started doing the shotFired fucntion.

--- src/objects/gameboard.js
+++ src/objects/gameboard.js
@@ -155,4 +155,8 @@ export default class GameBoard {
     }
   }
  }
+  shotFired(coordX, coordY) {
+   
+  }
+
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -39,12 +39,82 @@ test("A ship has been placed", () => {
 }) 
 
 test("A ship has been placed horizontally", () => {
-    expect(testBoard.placeShip(3,3,0,"H")).toStrictEqual(
+    expect(testBoard.placeShip(3,1,5,"H")).toStrictEqual(
         [
-            ["H","o","o","o","o","V","V","V","V","V"],
-            ["H","o","o","o","o","o","o","o","o","o"],
-            ["H","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","V","V","V","V","V"],
+            ["o","o","o","o","o","H","o","o","o","o"],
+            ["o","o","o","o","o","H","o","o","o","o"],
+            ["o","o","o","o","o","H","o","o","o","o"],
             ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"]
+            ]
+    )
+})
+
+test("Ship can't go out of bounds", () => {
+    testBoard.placeShip(4,9,9,"V");
+    expect(testBoard.board).toStrictEqual(
+        [
+            ["o","o","o","o","o","V","V","V","V","V"],
+            ["o","o","o","o","o","H","o","o","o","o"],
+            ["o","o","o","o","o","H","o","o","o","o"],
+            ["o","o","o","o","o","H","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"]
+            ]
+    )
+})
+
+test("Can't place on already existing ship", () => {
+    testBoard.placeShip(4,0,6,"V");
+    expect(testBoard.board).toStrictEqual(
+        [
+            ["o","o","o","o","o","V","V","V","V","V"],
+            ["o","o","o","o","o","H","o","o","o","o"],
+            ["o","o","o","o","o","H","o","o","o","o"],
+            ["o","o","o","o","o","H","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"],
+            ["o","o","o","o","o","o","o","o","o","o"]
+            ]
+    )
+})
+
+test("Can't place already placed ship", () => {
+    testBoard.placeShip(5,2,0,"V");
+    expect(testBoard.board).toStrictEqual([
+        ["o","o","o","o","o","V","V","V","V","V"],
+        ["o","o","o","o","o","H","o","o","o","o"],
+        ["o","o","o","o","o","H","o","o","o","o"],
+        ["o","o","o","o","o","H","o","o","o","o"],
+        ["o","o","o","o","o","o","o","o","o","o"],
+        ["o","o","o","o","o","o","o","o","o","o"],
+        ["o","o","o","o","o","o","o","o","o","o"],
+        ["o","o","o","o","o","o","o","o","o","o"],
+        ["o","o","o","o","o","o","o","o","o","o"],
+        ["o","o","o","o","o","o","o","o","o","o"]
+        ])
+})
+
+test("Check if a ship has been hit", () => {
+    testBoard.shotFired(6,0);
+    expect(testBoard.board).toStrictEqual(
+        [
+            ["o","o","o","o","o","X","V","V","V","V"],
+            ["o","o","o","o","o","H","o","o","o","o"],
+            ["o","o","o","o","o","H","o","o","o","o"],
+            ["o","o","o","o","o","H","o","o","o","o"],
             ["o","o","o","o","o","o","o","o","o","o"],
             ["o","o","o","o","o","o","o","o","o","o"],
             ["o","o","o","o","o","o","o","o","o","o"],
